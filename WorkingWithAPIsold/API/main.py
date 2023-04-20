024a402a07fff1b494fa1e59a2b3e03354722d76
ganavijayaram <jayaram.g@northeastern.edu>
2023-04-20 07:00:56 +0530

    First commit

---
+++ WorkingWithAPIsold/API/main.py
@@ -0,0 +1,48 @@
+#fastapi is the module and FASTAPI is the class
+from fastapi import FastAPI 
+from fastapi.params import Body
+#For creating schema for the Post request
+from pydantic import BaseModel
+#For creating optional parameters for Post data
+from typing import Optional
+
+#Creating instance of the class FastAPI
+app = FastAPI()
+
+
+#Creating schema for the Post request
+class Post(BaseModel):
+    #title and content has to be present and has to be string datatype
+    title: str
+    content: str
+    #setting default value, in case the value is not provided
+    rating: Optional[int] = None
+    published: bool = True 
+
+
+
+#routing/path operations
+
+#This is a decorator, inside get(), we give the path so that root() os executed
+@app.get("/")
+#this is the function which gets executed when / is called on the web browser
+def root():
+    return {"message": "Ganavi got an internship!!"}
+
+
+@app.get("/posts")
+def getPosts():
+    return {"Returning the Posts"}
+
+
+#Here when the client will send data, the data is taken by the API
+#and API will send it to pur web app. so we are extracting that paylaod and returning 
+#the same data to the user(we can return anything, but we are returning the data which user used)
+ 
+@app.post("/createposts")
+def createPosts(post: Post):
+    print(post)
+    #converting to dict
+    print(post.dict())
+    return {"data": "new post"}
+    #return {"newpost": f"title: {payload['title']} content: {payload['content']}"}
