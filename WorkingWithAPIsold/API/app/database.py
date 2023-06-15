153e23a163304ea0c2b62d052ac43fbd6d57cce5
ganavijayaram <jayaram.g@northeastern.edu>
2023-06-15 17:17:26 -0400

    Moving the get_db function to database.py

--- WorkingWithAPIsold/API/app/database.py
+++ WorkingWithAPIsold/API/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker 
 
 #Creating connection string to the database
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:IAMBLESSEDthankyou1!@localhost/WorkingWithFastAPI'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:@localhost/WorkingWithFastAPI'
 
 #Creating an engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -14,3 +14,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 #class which is used by the all the models to define the tables
 Base = declarative_base()
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
