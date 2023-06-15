9b376a926dfa91a77090ebe0397006c7d32f6922
ganavijayaram <jayaram.g@northeastern.edu>
2023-06-15 17:26:05 -0400

    moved get_db to database.py

--- WorkingWithAPIsold/API/app/database.py
+++ WorkingWithAPIsold/API/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker 
 
 #Creating connection string to the database
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:@localhost/WorkingWithFastAPI'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:IAMBLESSEDthankyou1!@localhost/WorkingWithFastAPI'
 
 #Creating an engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
