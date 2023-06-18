d60c1e6e2a64ed25f4f48a2ad129efb421f4d53c
ganavijayaram <jayaram.g@northeastern.edu>
2023-06-18 14:46:19 -0400

    Adding the foreign key

--- WorkingWithAPIsold/API/app/models.py
+++ WorkingWithAPIsold/API/app/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 
@@ -12,6 +12,7 @@ class Post(Base):
     published = Column(Boolean, server_default = 'TRUE', nullable = False)
     created_at = Column(TIMESTAMP(timezone=True),
                          server_default = text('now()'), nullable = False)
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete = "CASCADE"), nullable = False)
     
 
 class User(Base):
