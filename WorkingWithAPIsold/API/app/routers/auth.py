e23fe6bf7156a11ac4a542c0648b62173efdfaae
ganavijayaram <jayaram.g@northeastern.edu>
2023-06-18 10:19:08 -0400

    Using OauthPassword Request form to send in the username and password

--- WorkingWithAPIsold/API/app/routers/auth.py
+++ WorkingWithAPIsold/API/app/routers/auth.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from .. import schemas, database, models, utils, oauth2
 from sqlalchemy.orm import Session
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(tags = ['Authentication'])
 
 @router.post("/login")
-def login(userCredentials: schemas.UserLogin,
+def login(userCredentials: OAuth2PasswordRequestForm = Depends(),
            db: Session = Depends(database.get_db)):
     
-    user = db.query(models.User).filter(models.User.email == userCredentials.email).first()
+    user = db.query(models.User).filter(models.User.email == userCredentials.username).first()
     if not user:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
                             detail = f"Invalid Credentials")
